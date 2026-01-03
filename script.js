dd1b3ab5bfa6aa994e51c6cb6955af786525a903
git-user-ui <pratikpp2302@gmail.com>
2026-01-03 19:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  const result = [];
+  let currentChunk = [];
+  let currentSum = 0;
+
+  for (let num of arr) {
+    // If adding num exceeds max sum, start new chunk
+    if (currentSum + num > n) {
+      result.push(currentChunk);
+      currentChunk = [];
+      currentSum = 0;
+    }
+
+    currentChunk.push(num);
+    currentSum += num;
+  }
+
+  // Push last chunk if not empty
+  if (currentChunk.length > 0) {
+    result.push(currentChunk);
+  }
+
+  return result;
 };
+
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
